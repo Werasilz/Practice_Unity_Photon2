48b8ba6c7b745d8f1012c96123c532f3a4ecc809
Werasil Rerkweang <zen_frame@hotmail.com>
2023-07-11 17:42:49 +0700

    Start Game

---
+++ Assets/Scripts/RoomManager.cs
@@ -0,0 +1,53 @@
+using Photon.Pun;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.IO;
+
+public class RoomManager : MonoBehaviourPunCallbacks
+{
+    public static RoomManager Instance;
+
+    private void Awake()
+    {
+        if (Instance)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(gameObject);
+        Instance = this;
+    }
+
+    private void OnDestroy()
+    {
+        if (Instance == this)
+        {
+            Instance = null;
+        }
+    }
+
+    public override void OnEnable()
+    {
+        base.OnEnable();
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    public override void OnDisable()
+    {
+        base.OnDisable();
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode loadSceneMode)
+    {
+        // Game Scene
+        if (scene.buildIndex == 1)
+        {
+            print("[Photon] Create Player Manager");
+            GameObject newPlayerManager = PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "[Photon]PlayerManager"), Vector3.zero, Quaternion.identity);
+            // PhotonView photonView = newPlayerManager.GetComponent<PhotonView>();
+            // newPlayerManager.name = newPlayerManager.name + "_" + photonView.Owner.NickName;
+        }
+    }
+}
