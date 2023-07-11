e761f625d603f5787471a12fc39a578f382d9879
Werasil Rerkweang <zen_frame@hotmail.com>
2023-07-11 15:40:33 +0700

    Create Room

--- Assets/Scripts/Launcher.cs
+++ Assets/Scripts/Launcher.cs
@@ -1,29 +1,70 @@
 using Photon.Pun;
+using TMPro;
+using UnityEngine;
 
 public class Launcher : MonoBehaviourPunCallbacks
 {
+    [Header("GUI")]
+    [SerializeField]
+    private TMP_InputField input_RoomName;
+    [SerializeField]
+    private TextMeshProUGUI text_RoomName;
+
     void Start()
     {
-        print("Connecting to Master");
+        print("[Photon] Connecting to Master");
         MenuManager.Instance.OpenMenu(MenuName.LoadingMenu);
         PhotonNetwork.ConnectUsingSettings();
     }
 
     public override void OnConnectedToMaster()
     {
-        print("Connected to Master");
+        print("[Photon] Connected to Master");
         PhotonNetwork.JoinLobby();
     }
 
     public override void OnJoinedLobby()
     {
-        MenuManager.Instance.CloseMenu(MenuName.LoadingMenu);
+        print("[Photon] Joined Lobby");
         MenuManager.Instance.OpenMenu(MenuName.TitleMenu);
-        print("Joined Lobby");
     }
 
-    void Update()
+    public void CreateRoom()
     {
+        if (string.IsNullOrEmpty(input_RoomName.text))
+        {
+            print("[Photon] Can't create room, room name is empty");
+            return;
+        }
 
+        print("[Photon] Creating Room: " + input_RoomName.text);
+        MenuManager.Instance.OpenMenu(MenuName.LoadingMenu);
+        PhotonNetwork.CreateRoom(input_RoomName.text);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        print("[Photon] Joined Room:" + PhotonNetwork.CurrentRoom.Name);
+        MenuManager.Instance.OpenMenu(MenuName.RoomMenu);
+        text_RoomName.text = "Room Name: " + PhotonNetwork.CurrentRoom.Name;
+    }
+
+    public override void OnCreateRoomFailed(short returnCode, string message)
+    {
+        print("[Photon] Room Creation Failed: " + message);
+        MenuManager.Instance.OpenMenu(MenuName.RoomFailedMenu);
+    }
+
+    public void LeaveRoom()
+    {
+        print("[Photon] Leaving Room:" + PhotonNetwork.CurrentRoom.Name);
+        PhotonNetwork.LeaveRoom();
+        MenuManager.Instance.OpenMenu(MenuName.LoadingMenu);
+    }
+
+    public override void OnLeftRoom()
+    {
+        print("[Photon] Leave Room");
+        MenuManager.Instance.OpenMenu(MenuName.TitleMenu);
     }
 }
--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -3,15 +3,50 @@ using UnityEngine;
 public class MenuManager : Singleton<MenuManager>
 {
     [SerializeField] private GameObject[] menus;
+    private GameObject currentOpenedMenu;
 
+    // Open menu by index (Enum)
     public void OpenMenu(MenuName menuName)
     {
+        // Close current menu
+        if (currentOpenedMenu != null)
+        {
+            CloseMenu(currentOpenedMenu);
+        }
+
+        // Open new menu
         menus[(int)menuName].SetActive(true);
+
+        // Set to current menu
+        currentOpenedMenu = menus[(int)menuName];
     }
 
+    // Open menu by gameObject
+    public void OpenMenu(GameObject menuObject)
+    {
+        // Close current menu
+        if (currentOpenedMenu != null)
+        {
+            CloseMenu(currentOpenedMenu);
+        }
+
+        // Open new menu
+        menuObject.SetActive(true);
+
+        // Set to current menu
+        currentOpenedMenu = menuObject;
+    }
+
+    // Close menu by index (Enum)
     public void CloseMenu(MenuName menuName)
     {
         menus[(int)menuName].SetActive(false);
+    }
+
+    // Close menu by gameObject
+    public void CloseMenu(GameObject menuObject)
+    {
+        menuObject.SetActive(false);
     }
 }
 
@@ -19,4 +54,7 @@ public enum MenuName
 {
     LoadingMenu,
     TitleMenu,
+    CreateRoomMenu,
+    RoomMenu,
+    RoomFailedMenu,
 }
