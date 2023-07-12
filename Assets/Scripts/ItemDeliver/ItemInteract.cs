324c9e40a30c62f072e93de25074bbd4be8d4af4
Werasil Rerkweang <zen_frame@hotmail.com>
2023-07-12 12:28:14 +0700

    Sync Destroy

--- Assets/Scripts/ItemDeliver/ItemInteract.cs
+++ Assets/Scripts/ItemDeliver/ItemInteract.cs
@@ -1,8 +1,15 @@
+using Photon.Pun;
 using UnityEngine;
 
 public class ItemInteract : MonoBehaviour, IInteractable
 {
+    private PhotonView photonView;
     public Item item;
+
+    private void Start()
+    {
+        photonView = GetComponent<PhotonView>();
+    }
 
     public void Interact(Interaction interaction, ItemHolder itemHolder)
     {
@@ -13,6 +20,19 @@ public class ItemInteract : MonoBehaviour, IInteractable
         // Set holding item
         interaction.SetHolderItem(newGoodsItem);
         itemHolder.SetActiveItemObject(true);
+
+        // Destroy Object
+        if (photonView.IsMine)
+        {
+            PhotonNetwork.Destroy(gameObject);
+        }
+
+        photonView.RPC("RPC_Destroy", RpcTarget.All);
+    }
+
+    [PunRPC]
+    private void RPC_Destroy()
+    {
         Destroy(gameObject);
     }
 }
--- Assets/Scripts/Player/IInteractable.cs
+++ Assets/Scripts/Player/IInteractable.cs
@@ -1,4 +1,4 @@
-using UnityEngine;
+using Photon.Pun;
 
 public interface IInteractable
 {
